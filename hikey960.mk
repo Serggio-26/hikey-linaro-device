e7f0e2159d1a87f241a68ae9502edafdc14fd04f
Serhii Diachenko <Sergey.Dyachenko@gmail.com>
2020-06-13 20:47:16 +0300

    Change hikey960.mk to build kernel during system build (not use prebuilt kernel)
    Signed-off-by: Serhii Diachenko <Sergey.Dyachenko@gmail.com>

--- hikey960.mk
+++ hikey960.mk
@@ -1,8 +1,11 @@
 ifndef TARGET_KERNEL_USE
-TARGET_KERNEL_USE=4.9
+  TARGET_KERNEL_USE=4.9
+else
+  $(error "Only 4.9 is supported")
 endif
-TARGET_PREBUILT_KERNEL := device/linaro/hikey-kernel/Image.gz-hikey960-$(TARGET_KERNEL_USE)
-TARGET_PREBUILT_DTB := device/linaro/hikey-kernel/hi3660-hikey960.dtb-$(TARGET_KERNEL_USE)
+
+TARGET_PREBUILT_KERNEL := out/target/product/hikey960/Image.gz-hikey960-4.9
+TARGET_PREBUILT_DTB := out/target/product/hikey960/hi3660-hikey960.dtb-4.9
 
 ifeq ($(TARGET_KERNEL_USE), 4.4)
   HIKEY_USE_LEGACY_TI_BLUETOOTH := true
